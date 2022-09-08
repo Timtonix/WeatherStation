a33f162c8842b7c7476601811937dea09fb24fdc
timtonix <timtonic@outlook.fr>
2022-09-08 21:07:07 +0200

    Passage des test de base, le serveur reçoit et répond aux messages

--- App/server/tests/test_server.py
+++ App/server/tests/test_server.py
@@ -2,16 +2,18 @@ import pytest
 import zmq
 
 
-class ServerTest:
-    def __init__(self):
-        self.context = zmq.Context()
-        self.socket = self.context.socket(zmq.REQ)
+class TestServer:
+    context = zmq.Context()
+    socket = context.socket(zmq.REQ)
+
     def fake_client(self):
-        self.socket.connect('tcp://localhost:4554')
+        TestServer.socket.connect('tcp://localhost:4554')
 
     def test_received_message(self):
-        ServerTest.fake_client()
-        assert self.socket.send(b'Hello') == "Thank you"
+        self.fake_client()
+        TestServer.socket.send(b'Hello')
+        response = TestServer.socket.recv()
+        assert response == b'Thank You'
 
 
 
