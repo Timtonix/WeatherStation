560a4666702228d9d7fd98b76d36ea3cb83a564f
TITERIC12 <griszou12@gmail.com>
2022-09-10 18:08:16 +0200

    création de la fonction qui récupère le chemin vers le fichier à traiter (get_path())

--- Traitement json/main.py
+++ Traitement json/main.py
@@ -1,5 +1,4 @@
 # imports
-import json
 import os
 
 
@@ -8,15 +7,3 @@ def get_path():
     dir_list = os.listdir("./min_json")
     file_path = dir_list[1]
     return file_path
-
-
-def read_json():
-    path = "./min_json/" + get_path()
-    with open(path) as json_file:
-        json_content = json_file.read()
-        with open("./day_json", "x") as newfile:
-            newfile.write(json_content)
-
-
-# main code
-read_json()
