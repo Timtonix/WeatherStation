52cd7e8368d1068d0987fdc64081f191e068d8ba
TITERIC12 <griszou12@gmail.com>
2022-09-10 18:13:47 +0200

    création de la fonction lit le fichier puis en retourne son contenu (read_json())

--- Traitement json/main.py
+++ Traitement json/main.py
@@ -7,3 +7,13 @@ def get_path():
     dir_list = os.listdir("./min_json")
     file_path = dir_list[1]
     return file_path
+
+
+def read_json():
+    path = "./min_json/" + get_path()
+    with open(path, "r") as json_file:
+        json_content = json_file.read()
+    return json_content
+
+
+# main code
