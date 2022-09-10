e543dafff1f98e3464c376e8916ad64dc43a5c09
TITERIC12 <griszou12@gmail.com>
2022-09-10 19:45:14 +0200

    création de la fonction qui permet de supprimer un fichier (del_file()) (utile après la création du nouveau fichier(cette fonction n'est pas encore faite))

--- Traitement json/main.py
+++ Traitement json/main.py
@@ -1,19 +1,21 @@
 # imports
-import os
-
+import os, datetime
 
 # functions
 def get_path():
     dir_list = os.listdir("./min_json")
-    file_path = dir_list[1]
+    file_path = "./min_json/" + dir_list[0]
     return file_path
 
 
 def read_json():
-    path = "./min_json/" + get_path()
+    path = get_path()
     with open(path, "r") as json_file:
         json_content = json_file.read()
     return json_content
 
+
+def del_file(file_path):
+    os.remove(file_path)
 
 # main code
