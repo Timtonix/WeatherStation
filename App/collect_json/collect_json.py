d7f5e4e492e8a6b3b9b8f5683994ce3c2af33e9e
timtonix <timtonic@outlook.fr>
2022-09-11 18:05:52 +0200

    Finish the collect_json.py Class

--- App/collect_json/collect_json.py
+++ App/collect_json/collect_json.py
@@ -10,6 +10,10 @@ class CollectJson:
         date_now = date_now.strftime("%d-%m-%y")
         return date_now
 
+    def get_timestamp(self):
+        timestamp = datetime.datetime.timestamp(datetime.datetime.now())
+        return timestamp
+
     def create_day_weather_file(self):
         dir = os.listdir('./day_weather_json')
         date = f"{self.get_date()}.json"
@@ -31,3 +35,20 @@ class CollectJson:
             json_content = json.loads(json_file.read())
             return json_content
 
+
+    def process_weather_files(self, weather_data, day_weather, timestamp):
+        day_weather["temp"][f"{timestamp}"] = weather_data["temp"]
+        day_weather["humidity"][f"{timestamp}"] = weather_data["humidity"]
+        return day_weather
+
+    def write_in_the_day_file(self, modified_day_weather):
+        with open(f"day_weather_json/{self.get_date()}.json", "w") as json_file:
+            json.dump(modified_day_weather, json_file, indent=4)
+        return modified_day_weather
+
+    def main(self):
+        timestamp = self.get_timestamp()
+        weather_file = self.load_weather_file()
+        day_weather_file = self.load_day_weather_file()
+        process = self.process_weather_files(weather_file, day_weather_file, timestamp)
+        return self.write_in_the_day_file(process)
--- App/collect_json/test_collect_json.py
+++ App/collect_json/test_collect_json.py
@@ -30,4 +30,24 @@ class TestCollectJson:
         else:
             assert False
 
+    def test_process_weather_files(self):
+        timestamp = collect_json.get_timestamp()
+        weather_data = {"temp": 30, "humidity": 156}
+        day_weather = {"temp": {"today": 26}, "humidity": {"today": 164}}
+        process = collect_json.process_weather_files(weather_data, day_weather, timestamp)
+        modified_day_weather = {"temp": {f"{timestamp}": 30, "today": 26}, "humidity": {f"{timestamp}": 156, "today": 164}}
+        assert modified_day_weather == process
 
+    def test_write_in_the_day_file(self):
+        timestamp = collect_json.get_timestamp()
+        modified_day_weather = {"temp": {f"{timestamp}": 30, "today": 26}, "humidity": {f"{timestamp}": 156, "today": 164}}
+        collect_json.write_in_the_day_file(modified_day_weather)
+        with open(f"day_weather_json/{collect_json.get_date()}.json", "r") as json_file:
+            json_content = json.loads(json_file.read())
+            assert json_content == modified_day_weather
+
+    def test_main(self):
+        writed = collect_json.main()
+        with open(f"day_weather_json/{collect_json.get_date()}.json", "r") as json_file:
+            json_content = json.loads(json_file.read())
+        assert writed == json_content
