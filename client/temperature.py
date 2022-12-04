fbe5db20333c56a236b5ef92738358f3b4673ab3
timtonix <timtonic@outlook.fr>
2022-12-04 18:29:55 +0100

    modify the client and DHT sensor

--- client/temperature.py
+++ client/temperature.py
@@ -1,21 +1,25 @@
 import machine
 import dht
 
-def setup_dht_sensor(pin):
-    d = dht.DHT11(machine.Pin(pin))
-    while flag == 0:
-        try:
-            d.measure()
-            print("It's work !")
-            flag = 1
-        except OSError:
-            flag = 0
-            print("Something wrong with d.measure()")
 
-def get_temperature():
-    return d.temperature()
+class DHT_sensor:
 
-def get_humidity():
-    return d.humidity()
+    def __init__(self, pin):
+        self.d = dht.DHT11(machine.Pin(pin))
+        flag = 0
+        while flag == 0:
+            try:
+                self.d.measure()
+                print("It's work !")
+                flag = 1
+            except OSError:
+                flag = 0
+                print("Something wrong with d.measure()")
+
+    def get_temperature(self):
+        return self.d.temperature()
+
+    def get_humidity(self):
+        return self.d.humidity()
 
 
