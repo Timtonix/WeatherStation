98bd1856901c6f616dc9eb8ff8c521680944b6b5
timtonix <timtonic@outlook.fr>
2022-12-05 21:09:31 +0100

    Add the thread version !!! It works fine but needs refactoring

---
+++ server/server_thread.py
@@ -0,0 +1,44 @@
+# import socket programming library
+import socket
+from collect_json import *
+import threading
+
+
+def process_client(client):
+    while True:
+        request = client.recv(1024)
+        request = request.decode('utf-8')
+        print(request)
+        if not request:
+            print("Je m'arrête y'a R")
+            break
+        message = "Continue"
+        message = message.encode('utf-8')
+
+        try:
+            request = json.loads(request)
+            collect_json.main(request)
+            client.send(message)
+        except ValueError:
+            print("Client send a false JSON")
+            message = "wtf"
+            message = message.encode('utf-8')
+            client.send(message)
+
+
+socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+socket.bind(("", 4554))
+socket.listen(1)
+print("I'm listening")
+collect_json = CollectJson()
+
+while True:
+    print("waiting...")
+    client, address = socket.accept()
+    print(f"Connect with {address}")
+    request = threading.Thread(target=process_client, args=(client,))
+    request.start()
+    print("Just for testing")
+    request.join()
+    print("Finish all the code")
+
